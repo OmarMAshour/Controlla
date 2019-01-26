5c1098ed009fe07c2439bd8003a92c9ff6117749
farida-abouish <32582370+farida-abouish@users.noreply.github.com>
2019-01-26 21:55:56 +0200

    logIn page

--- app/src/main/java/com/controlla/controlla/MainActivity.java
+++ app/src/main/java/com/controlla/controlla/MainActivity.java
@@ -5,17 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
     private Button b1;
+    private EditText name,ps;
+    private CheckBox cb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1=(Button) findViewById(R.id.button2);
+        b1=(Button) findViewById(R.id.login);
+        name=(EditText) findViewById(R.id.username);
+        ps=(EditText) findViewById(R.id.password);
+        cb=(CheckBox) findViewById(R.id.checkBox);
 
         b1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
