8d82a688a9bf9cf7a7a2b1a27f659796d13e88a1
Omar Mohamed Ashour <omar_m_ashour@yahoo.com>
2019-06-24 22:10:47 +0200

    create class for service

---
+++ app/src/main/java/Services/BackgroundOBDCheck.java
@@ -0,0 +1,67 @@
+package Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.controlla.controlla.R;
+
+public class BackgroundOBDCheck extends Service {
+    private static final String TAG = null;
+    MediaPlayer player;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        player = MediaPlayer.create(this, R.raw.organfinale);
+//        player.setLooping(true); // Set looping
+//        player.setVolume(100,100);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.img)
+                .setContentTitle("HELP")
+                .setContentText("They stole my money")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+    }
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return 1;
+    }
+
+    public void onStart(Intent intent, int startId) {
+        // TO DO
+        player.start();
+    }
+    public IBinder onUnBind(Intent arg0) {
+        // TO DO Auto-generated method
+        return null;
+    }
+
+    public void onStop() {
+
+    }
+    public void onPause() {
+
+    }
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+}
